41d1c7bca706aff8686728ef12b157e1e8fade5b
Tabre <ktabrez791@gmail.com>
2025-06-30 21:11:13 +0530

    add this file

---
+++ Application.java
@@ -0,0 +1,14 @@
+package com.example.my_first_project.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+                 System.out.println(" Hello java world");
+	}
+
+}
